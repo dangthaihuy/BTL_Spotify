11ae08ee09194d92118a20b7ef9d594fcd1e3fb4
thuyquynh2508 <thuyquynh404@gmail.com>
2022-04-20 01:31:26 +0700

    done playscreen mobile tablet

---
+++ assets/js/playmob.js
@@ -0,0 +1,26 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const playBar = $('.main-footer')
+const playScreen = $('.root__playing')
+const downIcon = $('.playing-song-content-icon-mob')
+
+playBar.onclick = function (e) {
+    const playClick = e.target.closest('.playing-control__buttons-play');
+    const pauseClick = e.target.closest('.playing-control__buttons-pause');
+    if (playClick || pauseClick) {
+        return;
+    } else {
+        playScreen.classList.remove('res-hide');
+        playBar.classList.add('res-hide');
+        playScreen.classList.add('play-mob');
+        downIcon.classList.remove('res-hide')
+    }
+}
+
+downIcon.onclick = function() {
+    playScreen.classList.add('res-hide');
+    playBar.classList.remove('res-hide');
+    playScreen.classList.remove('play-mob');
+    downIcon.classList.add('res-hide')
+}
