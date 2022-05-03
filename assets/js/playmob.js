0bce76fc6a938a18151a46658ca3fb325d492afc
chn24 <huynghia24082002@gmail.com>
2022-05-03 10:56:03 +0700

    fix login and song-info

--- assets/js/playmob.js
+++ assets/js/playmob.js
@@ -4,6 +4,7 @@ const $$ = document.querySelectorAll.bind(document)
 const playBar = $('.main-footer')
 const playScreen = $('.root__playing')
 const downIcon = $('.playing-song-content-icon-mob')
+const root = document.getElementById("root")
 
 playBar.onclick = function (e) {
     const playClick = e.target.closest('.playing-control__buttons-play');
@@ -14,7 +15,8 @@ playBar.onclick = function (e) {
         playScreen.classList.remove('res-hide');
         playBar.classList.add('res-hide');
         playScreen.classList.add('play-mob');
-        downIcon.classList.remove('res-hide')
+        downIcon.classList.remove('res-hide');
+        root.setAttribute("class", "song-info")
     }
 }
 
@@ -22,5 +24,6 @@ downIcon.onclick = function() {
     playScreen.classList.add('res-hide');
     playBar.classList.remove('res-hide');
     playScreen.classList.remove('play-mob');
-    downIcon.classList.add('res-hide')
+    downIcon.classList.add('res-hide');
+    root.removeAttribute("class")
 }
